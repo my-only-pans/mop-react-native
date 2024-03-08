67670c31921884c048f01bd770531b41139380b9
Hector Robles <h_robles@fanshaweonline.ca>
2024-03-07 19:11:55 -0500

    skeleton layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,28 @@
 import { Slot } from "expo-router";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import Sidebar from "../components/Sidebar";
+import { Platform, StyleSheet, View } from "react-native";
 
 export default function HomeLayout() {
   return (
-    <>
+    <View style={styles.mainContainer}>
       <Header />
-      <Slot />
-      <Footer />
-    </>
+      <View style={styles.container}>
+        {Platform.OS === "web" ? <Sidebar /> : null}
+        <Slot />
+      </View>
+      {/* <Footer /> */}
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+  },
+  container: {
+    flexDirection: "row",
+    flex: 1,
+  },
+});
