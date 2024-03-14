f3347b23ef10500e836f436f0a8b1bdf02c8a96b
Hector Robles <h_robles@fanshaweonline.ca>
2024-03-14 00:30:58 -0400

    fix register page layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,7 +2,7 @@ import { Slot } from "expo-router";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Sidebar from "../components/Sidebar";
-import { Platform, StyleSheet, View } from "react-native";
+import { Platform, ScrollView, StyleSheet, View } from "react-native";
 
 export default function HomeLayout() {
   return (
@@ -10,7 +10,11 @@ export default function HomeLayout() {
       <Header />
       <View style={styles.container}>
         {Platform.OS === "web" ? <Sidebar /> : null}
-        <Slot />
+        <View style={styles.content}>
+          <ScrollView>
+            <Slot />
+          </ScrollView>
+        </View>
       </View>
       {/* <Footer /> */}
     </View>
@@ -20,9 +24,14 @@ export default function HomeLayout() {
 const styles = StyleSheet.create({
   mainContainer: {
     flex: 1,
+    backgroundColor: "#F2F7F5",
   },
   container: {
     flexDirection: "row",
+    flex: 1,
+    alignItems: "stretch",
+  },
+  content: {
     flex: 1,
   },
 });
