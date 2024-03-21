351a1387c31f511c007ec602cd898f2da274abcd
Hector Robles <h_robles@fanshaweonline.ca>
2024-03-21 02:24:45 -0400

    remove register default values, update env names

--- env.d.ts
+++ env.d.ts
@@ -1,4 +1,4 @@
 declare module "@env" {
-  export const EXPO_PUBLIC_WEB_SERVER_URL: string;
-  export const EXPO_PUBLIC_MOBILE_SERVER_URL: string;
+  export const WEB_SERVER_URL: string;
+  export const MOBILE_SERVER_URL: string;
 }
