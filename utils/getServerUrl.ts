20c576e420d0be3085de2aa553d2d4d8f87d7357
Hector Robles <h_robles@fanshaweonline.ca>
2024-03-21 02:20:42 -0400

    fix env issues

--- utils/getServerUrl.ts
+++ utils/getServerUrl.ts
@@ -1,9 +1,14 @@
+import {
+  EXPO_PUBLIC_WEB_SERVER_URL,
+  EXPO_PUBLIC_MOBILE_SERVER_URL,
+} from "@env";
+
 import { Platform } from "react-native";
 
 export default function getServerUrl() {
   if (Platform.OS === "web") {
-    return process.env.EXPO_PUBLIC_WEB_SERVER_URL;
+    return EXPO_PUBLIC_WEB_SERVER_URL;
   } else {
-    return process.env.EXPO_PUBLIC_MOBILE_SERVER_URL;
+    return EXPO_PUBLIC_MOBILE_SERVER_URL;
   }
 }
