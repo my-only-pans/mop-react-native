219414885b285ca8e93ca2986652c095e66184d0
Hector Robles <h_robles@fanshaweonline.ca>
2024-03-23 20:54:15 -0400

    update recipe card

--- app/recipes/index.tsx
+++ app/recipes/index.tsx
@@ -1,22 +1,10 @@
-import { Link } from "expo-router";
 import React from "react";
-import { Text, View } from "react-native";
-import TermsAndConditionsPage from "../../components/pages/privacy/Terms_&_Coditions";
-import Container from "../../components/commonComponents/Container";
+import RecipesPage from "../../components/pages/recipes/RecipesPage";
 
 interface Props {}
 
 function Recipes(props: Props) {
-  const {} = props;
-
-  return (
-    <Container>
-      <Text>This is the Recipes Screen</Text>
-      <Link href="/">
-        <Text>Home</Text>
-      </Link>
-    </Container>
-  );
+  return <RecipesPage />;
 }
 
 export default Recipes;
--- types/RecipeTypes.ts
+++ types/RecipeTypes.ts
@@ -2,4 +2,13 @@ export interface RecipeItemType {
   _id: string;
   name: string;
   imageUrl?: string;
+  owner?: {
+    _id: string;
+    username: string;
+  };
+  rating?: {
+    avg: number;
+    ratingNum: number;
+  };
+  createdAt?: string;
 }
