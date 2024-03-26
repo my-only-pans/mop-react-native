4d6482b58c831ff3b017a6f637de068238c0a740
Hector Robles <h_robles@fanshaweonline.ca>
2024-03-25 20:49:06 -0400

    update test route for api testing

--- app/test/index.tsx
+++ app/test/index.tsx
@@ -1,60 +1,98 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
-import { ScrollView, Text, View } from "react-native";
+import React, { useState } from "react";
+import { View } from "react-native";
+import { Button, TextInput } from "react-native-paper";
+import RNPickerSelect from "react-native-picker-select";
+import getServerUrl from "../../utils/getServerUrl";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 interface Props {}
 
-interface User {
-  _id: string;
-  username: string;
-}
-
-const sampleData = {
-  _id: "1",
-  username: "testUser1",
-};
-
 function Test(props: Props) {
+  const [route, setRoute] = useState<string>("");
+  const [method, setMethod] = useState("get");
+  const [payload, setPayload] = useState<string>("");
+  const [sendAuth, setSendAuth] = useState(true);
   const {} = props;
-  const [loading, setLoading] = useState(true);
-  const [user, setUser] = useState<User | null>(null); // Specify User type or null
 
-  const fetchUser = async () => {
-    const res = await axios.get("http://localhost:3000/test", {
-      params: {
-        _id: "65c576efa33aa4d3a65ac68d",
-      },
-    });
+  const handlePost = async () => {
+    try {
+      // Retrieve the authentication token from AsyncStorage
+      const authToken = await AsyncStorage.getItem("authToken");
 
-    setUser(res.data);
-    setLoading(false);
+      // Set the request headers correctly
+      const headers = {
+        Authorization: `Bearer ${authToken}`,
+      };
+
+      // Make the POST request
+      const res = await axios.post(
+        getServerUrl() + route,
+        {
+          body: payload && JSON.parse(payload),
+        },
+        { headers }
+      );
+
+      // Log the response data
+      console.log(res.data);
+    } catch (error) {
+      // Handle errors
+      console.log("ERROR", error);
+    }
   };
 
-  useEffect(() => {
-    fetchUser();
-  }, []);
+  const handleGet = async () => {
+    const authToken = await AsyncStorage.getItem("authToken");
 
-  if (loading) {
-    return <Text>Loading...</Text>;
-  }
+    try {
+      const res = await axios.get(getServerUrl() + route, {
+        params: payload && JSON.parse(payload),
+        headers: {
+          Authorization: `Bearer ${authToken}`,
+        },
+      });
 
-  if (!user) {
-    return (
-      <View>
-        <Text>No User found</Text>
-      </View>
-    );
-  }
+      console.log(res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleSend = async () => {
+    switch (method) {
+      case "get":
+        handleGet();
+        break;
+
+      case "post":
+        handlePost();
+        break;
+
+      default:
+        break;
+    }
+  };
 
   return (
     <View>
-      <Text>This is the test route</Text>
-      <View>
-        <View>
-          <Text>{user._id}</Text>
-          <Text>{sampleData.username}</Text>
-        </View>
-      </View>
+      <TextInput placeholder="route" value={route} onChangeText={setRoute} />
+      <RNPickerSelect
+        value={method}
+        onValueChange={setMethod}
+        items={[
+          { label: "GET", value: "get" },
+          { label: "POST", value: "post" },
+          { label: "PUT", value: "put" },
+          { label: "DELETE", value: "delete" },
+        ]}
+      />
+      <TextInput
+        placeholder="payload"
+        value={payload}
+        onChangeText={(value) => setPayload(value)}
+      />
+      <Button onPress={handleSend}>Send</Button>
     </View>
   );
 }
