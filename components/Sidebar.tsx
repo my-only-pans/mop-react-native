eaab370d9a261126f9f206d074ec7036ef224b89
Hector Robles <h_robles@fanshaweonline.ca>
2024-04-01 00:52:48 -0400

    update sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,50 +1,66 @@
 import { Link } from "expo-router";
-import React from "react";
-import { Platform, StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { Platform, Pressable, StyleSheet, Text, View } from "react-native";
 import colors from "../theme/colors";
-
-interface Props {}
+import { Icon } from "react-native-elements";
 
 const routes = [
   { href: "/recipes", label: "Recipes" },
   { href: "/user/profile/kitchen", label: "MyKitchen" },
-  { href: "/user/profile", label: "MyProfile" },
   { href: "/about", label: "About Us" },
   { href: "/contact", label: "Contact Us" },
   { href: "/partners", label: "Partners" },
   { href: "/privacy", label: "Privacy" },
-  { href: "/user/login", label: "Login" },
-  { href: "/user/register", label: "Register" },
   { href: "/recipes/new", label: "Create Recipe" },
-  { href: "/recipes/update", label: "Update Recipe" },
-  { href: "/test", label: "Test" },
   { href: "/recipes/myrecipes", label: "My Recipes" },
-  { href: "/recipes/view", label: "View Sample Recipe" },
 ];
 
 const styles = StyleSheet.create({
   container: {
     borderRightWidth: 1,
-    borderRightColor: Platform.OS === "web" ? colors.primary : "transparent",
-    padding: 32,
-    width: 300,
+    borderRightColor: Platform.OS === "web" ? colors.grey : "transparent",
+    justifyContent: "flex-end",
+  },
+  links: {
+    paddingVertical: 32,
+    flex: 1,
+    rowGap: 24,
+  },
+  linkContainer: {
+    paddingHorizontal: 32,
     alignItems: "flex-start",
   },
-  link: {
-    marginBottom: 32,
+  link: {},
+  expandBtn: {
+    width: "100%",
+    backgroundColor: colors.info,
   },
 });
 
-function Sidebar(props: Props) {
-  const {} = props;
+function Sidebar() {
+  const [isExpanded, setIsExpanded] = useState(false);
 
   return (
-    <View style={styles.container}>
-      {routes.map(({ href, label }) => (
-        <Link key={href} href={href} style={styles.link}>
-          <Text>{label}</Text>
-        </Link>
-      ))}
+    <View style={[styles.container, { width: isExpanded ? 250 : 60 }]}>
+      <View style={[styles.links, { display: isExpanded ? "flex" : "none" }]}>
+        {routes.map(({ href, label }) => (
+          <View key={href} style={styles.linkContainer}>
+            <Link href={href} style={styles.link}>
+              <Text>{label}</Text>
+            </Link>
+          </View>
+        ))}
+      </View>
+      <Pressable
+        style={styles.expandBtn}
+        onPress={() => setIsExpanded(!isExpanded)}
+      >
+        <Icon
+          name={`chevron-${isExpanded ? "left" : "right"}`}
+          color={colors.highlight}
+          size={48}
+        />
+      </Pressable>
     </View>
   );
 }
