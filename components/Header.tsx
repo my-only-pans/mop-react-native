e0e1bda97c2fd272946661ecb27ce9703dba82db
Hector Robles <h_robles@fanshaweonline.ca>
2024-04-04 15:30:00 -0400

    fix header logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,31 +1,11 @@
 import React from "react";
-import { Platform, Pressable, StyleSheet, Text, View, Image } from "react-native";
+import { Platform, Pressable, StyleSheet, View, Image } from "react-native";
 import colors from "../theme/colors";
 import { Avatar } from "react-native-paper";
 import { Link, useRouter } from "expo-router";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-interface Props {}
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: Platform.OS === "web" ? colors.primary : "transparent",
-    paddingVertical: 16,
-    paddingHorizontal: 36,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-  },
-  logo: { color: colors.highlight, fontWeight: "900", fontSize: 24 },
-  logoImage: { 
-    //height: Platform.OS === "web" ? 50 : 35, 
-    width: Platform.OS === "web" ? 220 : 130,
-    resizeMode: 'contain', 
-},
-});
-
-function Header(props: Props) {
-  const {} = props;
+function Header() {
   const router = useRouter();
 
   const handleClickAvataer = async () => {
@@ -42,11 +22,12 @@ function Header(props: Props) {
   return (
     <View style={styles.container}>
       <Link href="/">
-        {/* <Text style={styles.logo}>MyOnlyPans</Text> */}
-        <Image
-        style={styles.logoImage}
-        source={require('../assets/full-yellow.png')}
-      />
+        <View style={styles.logoContainer}>
+          <Image
+            source={require("../assets/full-yellow.png")}
+            style={styles.logo}
+          />
+        </View>
       </Link>
       <Pressable onPress={handleClickAvataer}>
         <Avatar.Image
@@ -57,5 +38,24 @@ function Header(props: Props) {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    backgroundColor: Platform.OS === "web" ? colors.primary : "transparent",
+    paddingVertical: 16,
+    paddingHorizontal: 36,
+  },
+  logoContainer: {
+    justifyContent: "center",
+  },
+  logo: {
+    width: Platform.OS === "web" ? 220 : 130,
+    height: 50,
+    resizeMode: "contain",
+  },
+});
 
 export default Header;
